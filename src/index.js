7f939a095d2ad413896066cf91f1c2c402ae0a9a
Harry Difolco <h@difol.co>
2020-04-01 05:30:52 +0100

    tweaking css

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ function Project(props) {
       </div>
       <div className="content">
         <div className="text">
-          <h4 className="projectDesc">{props.title}</h4>
+          <h5 className="projectDesc">{props.title}</h5>
           <p className="projectDesc">{props.desc}</p>
           <Chip tech={props.tech} />
         </div>
@@ -54,68 +54,6 @@ function Chip(props) {
 
   return chipList;
 }
-
-// class Board extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       squares: Array(9).fill(null)
-//     };
-//   }
-
-//   handleClick(i) {
-//     const squares = this.state.squares.slice();
-
-//     if (calculateWinner(squares) || squares[i]) {
-//       return;
-//     }
-//     squares[i] = this.state.xIsNext ? "X" : "O";
-//     this.setState({
-//       squares: squares,
-//       xIsNext: !this.state.xIsNext
-//     });
-//   }
-
-//   renderSquare(i) {
-//     return (
-//       <Square
-//         value={this.state.squares[i]}
-//         onClick={() => this.handleClick(i)}
-//       />
-//     );
-//   }
-
-//   render() {
-//     const winner = calculateWinner(this.state.squares);
-//     let status;
-//     if (winner) {
-//       status = "Winner: " + winner;
-//     } else {
-//       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-//     }
-
-//     return (
-//       <div>
-//         <div className="status">{status}</div>
-//         <div className="board-row">
-//           {this.renderSquare(0)}
-//           {this.renderSquare(1)}
-//           {this.renderSquare(2)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(3)}
-//           {this.renderSquare(4)}
-//           {this.renderSquare(5)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(6)}
-//           {this.renderSquare(7)}
-//           {this.renderSquare(8)}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 class Projects extends React.Component {
   render() {
