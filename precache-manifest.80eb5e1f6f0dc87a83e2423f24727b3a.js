63e17211e5027c9aaad7b5000da7874d77a36c62
Harry Difolco <h@difol.co>
2020-05-13 06:51:01 +0100

    Updates

---
+++ precache-manifest.80eb5e1f6f0dc87a83e2423f24727b3a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd492ec366545932a983b3a7550ace5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ee337a956b5a1d51bcb",
+    "url": "/static/css/main.9d16a1ab.chunk.css"
+  },
+  {
+    "revision": "078b7cb88e9a0925e603",
+    "url": "/static/js/2.878969ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.878969ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee337a956b5a1d51bcb",
+    "url": "/static/js/main.19448ef9.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "9bee367664598088db882ffc1dbbed4e",
+    "url": "/static/media/ludio.9bee3676.png"
+  },
+  {
+    "revision": "08b96d9e9bda94645df9b94bc8afb0e2",
+    "url": "/static/media/me.08b96d9e.png"
+  },
+  {
+    "revision": "564e2c785eeb59a51a7b3388ef736498",
+    "url": "/static/media/miro.564e2c78.png"
+  },
+  {
+    "revision": "a8aab1e4db53226b70c59876f5a00199",
+    "url": "/static/media/nt.a8aab1e4.png"
+  },
+  {
+    "revision": "c77a4765ed450c6b886add2532218c83",
+    "url": "/static/media/recipes.c77a4765.png"
+  },
+  {
+    "revision": "59ab1e5a2d0239a0eec8172b6b021fa1",
+    "url": "/static/media/tfl.59ab1e5a.png"
+  }
+]);
